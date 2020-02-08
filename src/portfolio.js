acd15e77f90139501628cd7180441bbeed59ac32
abhishek-singh-hub <59343539+abhishek-singh-hub@users.noreply.github.com>
2020-02-08 17:49:42 +0530

    portfolio Page
    static commit for portfolio Page

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './aa.css';
 import p1 from './photos/Portrait/p1.jpeg';
-import p2 from './photos/Portrait/p2.jpeg';
-import p3 from './photos/Portrait/p3.jpeg';
+import e1 from './photos/Events/e1.jpeg';
+import e3 from './photos/Events/e3.jpeg';
+import e4 from './photos/Events/e4.jpeg';
+import c4 from './photos/Corporate/c4.jpeg';
 import p4 from './photos/Portrait/p4.jpeg';
 import p5 from './photos/Portrait/p5.jpeg';
 import p6 from './photos/Portrait/p6.jpeg';
-import p7 from './photos/Portrait/p7.jpeg';
+import c8 from './photos/Corporate/c8.jpeg';
 import a1 from './photos/Admin/A1.jpeg';
 import log1 from './photos/logo1.jpg';
 
@@ -27,16 +29,16 @@ function portfolio(){
 				<div id="slider">
 					<figure>
 						<img src={p1}/>
-						<img src={p2}/>
-						<img src={p3}/>
-						<img src={p4}/>
-						<img src={p5}/>
+						<img src={e1}/>
+						<img src={c4}/>
+						<img src={e4}/>
 						<img src={p6}/>
 					</figure>
 				</div>
-				
-				<h1><strong>Ravi Shankar</strong></h1>
-				<h3><strong>Photographer</strong></h3>	
+				<div>
+					<h1 className="nameTag"><strong>Ravi Shankar</strong></h1>
+					<h3 className="nameTag"id="nameTagId"><strong>Photographer</strong></h3>	
+				</div>
 				<div className="main" id='social'>
 					<h2><strong>Social</strong></h2>
 					<h3>Follow my work on <a href="https://www.instagram.com/ordeal_clicks/" target="_blank">Instagram</a></h3>
@@ -44,29 +46,27 @@ function portfolio(){
 					<div className="row">
 						<div className="column">
 							<div className="content">
-								<a href="corparate.html"><img src={p1}/></a>
+								<a href=""><img src={c8} style={{width:'100%',height:'100%'}}/></a>
 									<h3>Corparate</h3>
 							</div>
 						</div>
 						<div className="column">
 							<div className="content">
-								<a href="events.html"><img src={p2}/></a>
+								<a href=""><img src={e3} style={{width:'100%',height:'100%'}}/></a>
 									<h3>Events</h3>
 							</div>
 						</div>
 						<div className="column">
 							<div className="content">
-								<a href="portrait.html"><img src={p3}/></a>
+								<a href=""><img src={p4} style={{width:'100%',height:'100%'}}/></a>
 									<h3>Portrait</h3>
 							</div>
 						</div>
-	
 					</div>
 				</div>
-				
-				
-				
 				<div>
+					<img src={a1} className="smaller-image2 image-border2" id='img1'/>		
+					<h3 id='about'><strong>About Me:</strong></h3>			
 					<h3>When words become unclear, I shall focus with photographs.
 					When images become inadequate, I shall be content with silence.The people who
 					hire me as a wedding photographer hire me not
